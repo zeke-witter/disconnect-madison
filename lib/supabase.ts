25b565c45fbc0526659d38580361209b7401b38d
Zekey <thevoiceofzeke@gmail.com>
2026-02-16 22:42:42 -0600

    use secret key for email verification

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_PUBLISHABLE_KEY!;
+const supabaseKey = process.env.SUPABASE_SECRET_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
